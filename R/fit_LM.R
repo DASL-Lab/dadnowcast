faf9e21d35fbf257fa153db5dca0d869aaa5a964
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2026-01-21 16:07:25 -0500

    Updated fit_LM to work with multiple explanatory variables and added test cases

--- R/fit_LM.R
+++ R/fit_LM.R
@@ -1,20 +1,22 @@
 #' Fit a linear model on given data and make predictions for a given set of data
-#' 
+#'
 #' @param XTrain Training data for the explanatory variables in the model
 #' @param YTrain Training data for the response variable
 #' @param XNowcast Data to make predictions bases on
-#' 
+#'
 #' @returns Linear model object and predictions
 #' @export
 
-fit_LM <- function(YTrain,XTrain, XNowcast){
-  
-  full_data <- as.data.frame(cbind(YTrain,XTrain))
+fit_LM <- function(Y_train, X_train, X_nowcast) {
+  full_data <- as.data.frame(cbind(Y_train, X_train))
 
-  fitted_LM <- lm(YTrain ~ ., data = full_data)
-  # fitted_LM
-  XNowcast <- as.data.frame(XNowcast)
-  predicted_LM <- predict(fitted_LM, XNowcast)
-  
+  fitted_LM <- lm(Y_train ~ ., data = full_data)
+
+  XNowcast <- as.data.frame(X_nowcast)
+
+  colnames(XNowcast) <- colnames(full_data)[-1]
+
+  predicted_LM <- predict(fitted_LM, newdata = XNowcast)
+
   list(fitted_LM, predicted_LM)
 }
--- tests/testthat/test-fit_LM.R
+++ tests/testthat/test-fit_LM.R
@@ -1,8 +1,27 @@
 test_that("fit simple model works", {
-  testY <- seq(1,10,1)
-  testX <- rnorm(10)
-  testNow <- rnorm(10)
-  test_mod <- fit_LM(testY,testX,testNow)
-  expect_equal(test_mod$coefficients[[1]], lm(testY ~ testX)$coefficients[[1]])
-  expect_equal(test_mod$coefficients[[2]], lm(testY ~ testX)$coefficients[[2]])
+  testY <- seq(1, 10, 1)
+  testX <- seq(1, 20, 2)
+  testNow <- c(1, 2, 3)
+  test_mod <- fit_LM(testY, testX, testNow)
+  expect_equal(test_mod[[1]]$coefficients[[1]], 0.5)
+  expect_equal(test_mod[[1]]$coefficients[[2]], 0.5)
+  expect_equal(test_mod[[2]][[1]], 1)
+  expect_equal(test_mod[[2]][[2]], 1.5)
+  expect_equal(test_mod[[2]][[3]], 2)
+})
+
+test_that("fitting simple model with multiple explanatory variables works", {
+  Y_train <- seq(1, 10, 1)
+  X_train <- data.frame(
+    x1 = c(1, -1.5, 2, 4, 5, 5.8, 6.4, 7.1, 8, 8.5),
+    x2 = c(1.838, 0.382, -0.306, 2.185, 0.458, -2.366, -0.959, -1.014, -0.158, -2.133)
+  )
+  X_nowcast <- data.frame(c(1, 2, 3), c(4, 5, 6))
+  test_mod <- fit_LM(Y_train, X_train, X_nowcast)
+  expect_equal(test_mod[[1]]$coefficients[[1]], 1.8348, tolerance = 0.001)
+  expect_equal(test_mod[[1]]$coefficients[[2]], 0.7736, tolerance = 0.001)
+  expect_equal(test_mod[[1]]$coefficients[[3]], -0.4033, tolerance = 0.001)
+  expect_equal(test_mod[[2]][[1]], 0.9950741, tolerance = 0.001)
+  expect_equal(test_mod[[2]][[2]], 1.3653039, tolerance = 0.001)
+  expect_equal(test_mod[[2]][[3]], 1.7355338, tolerance = 0.001)
 })
